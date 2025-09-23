a5c4668c8ddbdbf6aa86a9d35056489a62318169
Jesus-Eduardo-Perez <zs21007543@estudiantes.uv.mx>
2025-09-23 09:24:24 -0600

    Alineación correcta del Header

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
 import styles from '../CSS Components/Header.module.css'
+import Taller_logo from '../Images/Taller_logo.png'
 function Header () {
     return(
         <div className= {styles.Header}>
-            
+            <div className=  {styles.LogoContainer}>
+                <img width={167} height={200} src = {Taller_logo} alt="Taller carlitos"/>
+            </div>
+            <div className={styles.OptionsBar}>
+            </div>
         </div>
     );
 }
