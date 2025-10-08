a2ba44deaa14e747b92718767b365278d9d99c6a
Jesus-Eduardo-Perez <zs21007543@estudiantes.uv.mx>
2025-10-08 15:38:05 -0600

    Creación de pagina de admin, creacion de botones los cuales puede redirigir a otras páginas del administrador

--- frontend/src/Pages/Admin.js
+++ frontend/src/Pages/Admin.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import styles from '../CSS/Admin.module.css'
 import Header from '../Components/Header'
+import { useAsyncError, useNavigate } from "react-router-dom";
+import { getUserProfile } from "../Services/authService";
 function Admin() {
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({name: ''});
+    useEffect(() => {
+        const fetchUser = async () => {
+            const token = localStorage.getItem('token');
+            if(token){
+                const userData = await getUserProfile(token);
+                if (userData) {
+                    setUser(userData)
+                }
+            }
+        };
+
+        fetchUser();
+    }, []);
+    const options = [
+        { id: 1, nombre: "Marcas", ruta: "/admin/marcas"},
+        { id: 2, nombre: "Piezas", ruta: "/admin/piezas"}
+    ]
     return(
-        <div>
+        <div className= {styles.container}>
             <Header />
-            <h1>Aqui va la administración</h1>
+            <p>Bienvenido {user.name}</p>
+            <div className= {styles.container_admin}>
+                <h1>Opciones</h1>
+                <div className= {styles.brands_container}>
+                    {options.map(option => (
+                        <div className= {styles.buttonContainer}>
+                            <button 
+                           key={option.id}
+                           className = {styles.imageButton}
+                           onClick={() => {
+                            if(option.ruta){
+                                navigate(option.ruta)
+                            }
+                        }}>
+                        </button>
+                        <h4>{option.nombre}</h4>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            
         </div>
     );
 }
